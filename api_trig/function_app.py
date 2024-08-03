ceb5c15529d3489e3bc5ab009223ec5df1dee7dd
Justin Rainey <159929965+JRainey80@users.noreply.github.com>
2024-08-03 16:36:03 -0400

    Update function_app.py

--- api_trig/function_app.py
+++ api_trig/function_app.py
@@ -10,7 +10,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
        
     origin = req.headers.get("Origin")
-    allowed_origins = ["https://cdn-raineycloud.azureedge.net", "https://portal.azure.com", "https://raineyresume.z13.web.core.windows.net", "https://resumeapiapp.azurewebsites.net/api/api_trig"]  
+    allowed_origins = ["https://cdn-raineycloud.azureedge.net", "https://portal.azure.com", "https://raineyresume.z13.web.core.windows.net", "https://resumeapiapp.azurewebsites.net/api/api_trig", "https://resume.rainey-cloud.com"]  
 
     if origin not in allowed_origins:
         logging.warning(f"Unauthorized origin: {origin}")
