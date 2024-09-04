71f6a6e1e25238cbde470858bb127710a99ced06
jrainey80 <rainey.justin80@gmail.com>
2024-09-03 22:06:36 -0400

    more terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,92 @@ provider "azurerm" {
   # Configuration options
 }
 
-resource "azurerm_resource_group" "RG-Resume" {
-  name     = "Resume"
+data "azurerm_client_config" "current" {}
+
+
+resource "azurerm_resource_group" "RG-ResumeAPI" {
+  name     = "resume-api"
   location = "East US"
+}
+
+resource "azurerm_key_vault" "Key-Vault" {
+  name                        = "api55237"
+  location                    = azurerm_resource_group.RG-ResumeAPI.location
+  resource_group_name         = azurerm_resource_group.RG-ResumeAPI.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+
+    secret_permissions = ["Get", "List"]
+  }
+}
+
+#Function App API
+
+resource "azurerm_storage_account" "SA-ResumeAPI" {
+  name                     = "resumeapi9cbf"
+  resource_group_name      = azurerm_resource_group.RG-ResumeAPI.name
+  location                 = azurerm_resource_group.RG-ResumeAPI.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "App-Service-Plan" {
+  name                = "ASP-ResumeAPIapp-3fff"
+  location            = azurerm_resource_group.RG-ResumeAPI.location
+  resource_group_name = azurerm_resource_group.RG-ResumeAPI.name
+  os_type             = "Linux"
+  sku_name            = "S1"
+}
+
+resource "azurerm_linux_function_app" "example" {
+  name                = "example-function-app"
+  location            = azurerm_resource_group.RG-ResumeAPI.location
+  resource_group_name = azurerm_resource_group.RG-ResumeAPI.name
+  service_plan_id     = azurerm_service_plan.App-Service-Plan.id
+
+  storage_account_name       = azurerm_storage_account.SA-ResumeAPI.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+
+  site_config {
+    application_stack {
+      python_version = "3.9"
+    }
+  }
+}
+
+resource "azurerm_function_app_function" "example" {
+  name            = "example-function-app-function"
+  function_app_id = azurerm_linux_function_app.example.id
+  language        = "Python"
+  test_data = jsonencode({
+    "name" = "Azure"
+  })
+  config_json = jsonencode({
+    "bindings" = [
+      {
+        "authLevel" = "function"
+        "direction" = "in"
+        "methods" = [
+          "get",
+          "post",
+        ]
+        "name" = "req"
+        "type" = "httpTrigger"
+      },
+      {
+        "direction" = "out"
+        "name"      = "$return"
+        "type"      = "http"
+      },
+    ]
+  })
 }
